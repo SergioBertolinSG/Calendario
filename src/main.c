0ffd48bdea496d5127f5cfb81318417be4f95ff7
Sergio Bertolín <sbertolin@protonmail.com>
2026-05-03 22:42:45 +0200

    Remove dbus slot

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("io.github.SergioBertolinSG.Calendario", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("io.github.SergioBertolinSG.Calendario", G_APPLICATION_NON_UNIQUE);
     AppState *state = app_state_new();
     int status;
 
